b8629c5c2d7db4ca66e62be4c08f6ec08aed83b8
mfranl <mflandry09@gmail.com>
2021-01-28 18:55:38 -0700

    added grid to html and added a newfunction to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-console.log("hello")
+$(".saveBtn").on("click", function() {
+    var time = $(this).attr("id").split("-")[0]
+    var userEntry = $("#" + time + "-text").val()
+    console.log(time,userEntry)
+})
