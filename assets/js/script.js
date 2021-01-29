fca0f9d5230f4f458a17612d2a8a884c6ae27c03
mfranl <mflandry09@gmail.com>
2021-01-28 18:57:56 -0700

    saved variables to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,5 @@ $(".saveBtn").on("click", function() {
     var time = $(this).attr("id").split("-")[0]
     var userEntry = $("#" + time + "-text").val()
     console.log(time,userEntry)
+    localStorage.setItem(time, userEntry)
 })
