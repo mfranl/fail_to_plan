f8fd79d08c340bb5a9cf4cc1e30f29e9c204fb29
mfranl <mflandry09@gmail.com>
2021-01-30 10:26:00 -0700

    added some comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,20 @@
 var todayDate = moment().format('ddd, MMM, Do YYYY');
 $("#currentDay").html(todayDate);
 
-
-
-
-
-
-
-
+//save button event listener
 $(".saveBtn").on("click", function() {
     var time = $(this).attr("id").split("-")[0]
     var userEntry = $("#" + time + "-text").val()
-    console.log(time,userEntry)
+    //console.log(time,userEntry)
+    //saves to local storage 
     localStorage.setItem(time, userEntry)
 })
 
+//todays real time
 var hours = moment().hours()
-console.log(hours)
+//console.log(hours)
 
+//sets the correct background styling depending on time
 for (var i = 9; i <= 16; i++) {
     $("#" + i + "-text").val(localStorage.getItem(i))
     if (i < hours) {
